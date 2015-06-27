90e803fc387326705efae1ad1f4f5f7afe0e603c
gfcprogramer <gfcprogramer@hotmail.com>
2015-06-26 17:52:21 -0700

    Create a RuntimeOnly triangle. Set on MeshFilter. Set Camera On. Render.

---
+++ Assets/Scripts/VertexCreate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VertexCreate : MonoBehaviour {
+
+		public Vector3[] newVertices;
+		public Vector2[] newUV;
+		public int[] newTriangles;
+
+	void Start() {
+
+		Mesh mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+
+		newVertices[0] = new Vector3(0.1f, 0.5f, 0.5f);
+		newVertices[1] = new Vector3(0.5f, -0.5f, 0.5f);
+		newVertices[2] = new Vector3(-0.5f, -0.5f, 0.5f);
+
+		mesh.vertices = newVertices;
+		mesh.uv = newUV;
+		mesh.triangles = newTriangles;
+	}
+}
---
+++ Assets/Scripts/VertexUpdate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VertexUpdate : MonoBehaviour {
+	public Vector3[] newVertices;
+	public Vector2[] newUV;
+	public int[] newTriangles;
+	void Update() {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.Clear();
+		mesh.vertices = newVertices;
+		mesh.uv = newUV;
+		mesh.triangles = newTriangles;
+	}
+}
+
